08d2bd26fae069fcb4f7fddd0ea81b99d34a88da
Eduardo José Neto Oliveira <up202108843@up.pt>
2022-10-18 09:48:11 +0100

    Initial Verison

--- src/test/java/com/aor/numbers/ListSorterTest.java
+++ src/test/java/com/aor/numbers/ListSorterTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class ListSorterTest {
 
     public static List<Integer> list1(){
-        return Arrays.asList(3,5,2,4,6);
+        return Arrays.asList(3,5,2,4,1);
     }
     public static List<Integer> list2(){
         return Arrays.asList(4,3,2,1,5);
@@ -18,7 +18,7 @@ public class ListSorterTest {
     public void sort() {
         List<Integer> list = Arrays.asList(3, 2, 6, 1, 4, 5, 7);
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        List<Integer> expected1 = Arrays.asList(2,3,4,5,6);
+        List<Integer> expected1 = Arrays.asList(1,2,3,4,5);
         List<Integer> expected2 = Arrays.asList(1,2,3,4,5);
 
         ListSorter sorter = new ListSorter();
